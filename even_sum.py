729c14c5ded7e8e9c1c45470fe3097e5c451a09b
Robert Saunders Jr <robjrtech@gmail.com>
2026-08-20 15:51:35 -0500

    Completed the even_sum.py

---
+++ even_sum.py
@@ -0,0 +1,15 @@
+even_num_list = []
+
+for num in range(0,51):
+    if num % 2 == 0:
+        even_num_list.append(num)
+print(even_num_list)
+print(f'The sum of even numbers from 1 to 50 is: {sum(even_num_list)}')
+
+
+
+while num in range (0,51):
+    if num % 2 == 0:
+        even_num_list.append(num)
+print(even_num_list)
+print(f'The sum of even numbers from 1 to 50 is: {sum(even_num_list)}')
