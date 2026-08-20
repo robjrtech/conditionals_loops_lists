3b5471682553a77763f90b92c0bf3168a236587d
Robert Saunders Jr <robjrtech@gmail.com>
2026-08-20 15:29:13 -0500

    Completed grade checker.

---
+++ grade_checker.py
@@ -0,0 +1,17 @@
+grade = float(input("What was your pervious lab score from Python Essentials? "))
+
+if grade <= 0. or grade <= 59.9:
+    print('Your grade is: F')
+elif grade == 60. and grade <= 69.9:
+    print('Your grade is: D')
+elif grade >= 70. and grade <= 79.9:
+    print('Your grade is: C')
+elif grade >= 80. and grade <= 89.9:
+    print('Your grade is: B')
+elif grade >= 90:
+    print('Your grade is: A')
+if grade <= 0. or grade <= 69.9:
+    print('"Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time."\n -Thomas Edison"')
+else:
+    print("You worked for this. Enjoy every second of it.")
+
